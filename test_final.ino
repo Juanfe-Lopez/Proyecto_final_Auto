8735f16abb7dd23ab27f4e36113aeb920f940ff2
Juan Felipe López <63369671+Juanfe-Lopez@users.noreply.github.com>
2023-11-14 19:42:56 -0500

    Added comments test_final.ino

--- test_final.ino
+++ test_final.ino
@@ -3,28 +3,38 @@
 #include "MQ135.h"
 #include <LiquidCrystal.h>
 
-
+// Definiciones para el sensor DHT
 #define DHTPIN 52      // Pin de datos del DHT22
 #define DHTTYPE DHT22  // Tipo de sensor DHT
 DHT dht(DHTPIN, DHTTYPE);
 
+// Definiciones para la pantalla LCD
 const int rs = 24, en = 25, d4 = 34, d5 = 32, d6 = 30, d7 = 28;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+// Definiciones para el sensor de gas MQ135
 MQ135 gasSensor = MQ135(A0);
+
+// Definiciones para el relé y el ventilador
 const int relayPin = 22;               // Pin del relé
 const int ventiladorPin = 53;          // Pin del ventilador
-const float temperaturaUmbral = 35.0;  // Temperatura máxima permitida
 
+// Umbral de temperatura para encender la bombilla
+const float temperaturaUmbral = 35.0;
+
+// Umbrales para el CO2 y la humedad
+const int co2Umbral = 300;   // CO2 máxima permitida
+const int humedadUmbral = 65;   // Humedad máxima permitida
+
+// Variables para almacenar datos del entorno
 float temperatura = 0.0;
 float humedad = 0.0;
 float ppm = 0.0;
 
-
 void setup() {
   lcd.begin(16, 2);
   lcd.print("Control Temp");
-  Wire.begin();                // Dirección del esclavo
+  Wire.begin();                // Inicia la comunicación I2C
   pinMode(relayPin, OUTPUT);
   pinMode(ventiladorPin, OUTPUT);
   dht.begin();
@@ -34,10 +44,12 @@ void setup() {
 void loop() {
   delay(2000);  // Espera un momento entre lecturas
 
+  // Lee los datos del sensor DHT y MQ135
   temperatura = dht.readTemperature();
   humedad = dht.readHumidity();
   ppm = gasSensor.getPPM();
 
+  // Muestra los datos en la pantalla LCD
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
@@ -49,7 +61,7 @@ void loop() {
   lcd.print(humedad);
   lcd.print(" %");
 
-
+  // Muestra los datos en el puerto serie
   Serial.print("Temperatura: ");
   Serial.print(temperatura);
   Serial.println(" C");
@@ -70,25 +82,26 @@ void loop() {
     Serial.println("Apagar Bombilla");
   }
 
-  // Lógica para el ventilador basada en la concentración de CO2
-  if (ppm >= 113) {
+  // Lógica para el ventilador basada en la concentración de CO2 y humedad
+  if (ppm >= co2Umbral) {
     digitalWrite(ventiladorPin, HIGH);  // Enciende el ventilador
     Serial.println("¡Alerta! Concentración de CO2 alta. Encendiendo el ventilador.");
+  } else if (humedad >= humedadUmbral) {
+    digitalWrite(ventiladorPin, HIGH);  // Enciende el ventilador
   } else {
     digitalWrite(ventiladorPin, LOW);  // Apaga el ventilador
   }
-  Wire.beginTransmission(8);  // Dirección del maestro (ESP32)
-  Wire.write((uint8_t*)&temperatura, sizeof(temperatura));
-  Wire.write((uint8_t*)&humedad, sizeof(humedad));
-  Wire.write((uint8_t*)&ppm, sizeof(ppm));
-  Wire.endTransmission();
-  Serial.println("enviando");
+
+  // Envía los datos al otro microcontrolador a través de I2C
+  enviarDatos();
 }
+
+// Función para enviar datos a través de I2C
 void enviarDatos() {
   Wire.beginTransmission(8);  // Dirección del maestro (ESP32)
   Wire.write((uint8_t*)&temperatura, sizeof(temperatura));
   Wire.write((uint8_t*)&humedad, sizeof(humedad));
   Wire.write((uint8_t*)&ppm, sizeof(ppm));
   Wire.endTransmission();
-  Serial.println("enviando");
+  Serial.println("Enviando datos a través de I2C");
 }
